6427947fbe5beb2e3c13ace30c51269b1ea4d414
Topi <topi@ideamarket.fi>
2020-02-13 09:31:16 +0200

    dismiss undefined values, include null values in update expressions

--- src/db/updateExpressions.test.ts
+++ src/db/updateExpressions.test.ts
@@ -1,7 +1,7 @@
 import { createUpdateExpressions } from './updateExpressions'
 
 describe('Create DynamoDB update expressions for an item', () => {
-  test('Create update expression', () => {
+  test('Create update expressions with valid values', () => {
     const updatedItem = {
       name: 'test1',
       description: 'asdasd',
@@ -19,6 +19,49 @@ describe('Create DynamoDB update expressions for an item', () => {
         '#name': 'name',
         '#description': 'description',
         '#price': 'price'
+      }
+    }
+    expect(updateExpressions).toEqual(expected)
+  })
+
+  test('Create update expressions with some undefined values', () => {
+    const updatedItem = {
+      name: 'test1',
+      description: 'asdasd',
+      price: undefined
+    }
+    const updateExpressions = createUpdateExpressions(updatedItem)
+    const expected = {
+      UpdateExpression: 'SET #name = :name, #description = :description',
+      ExpressionAttributeValues: {
+        ':name': 'test1',
+        ':description': 'asdasd'
+      },
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#description': 'description'
+      }
+    }
+    expect(updateExpressions).toEqual(expected)
+  })
+  /** Expected to include null values in return object, but skip undefined values. */
+  test('Create update expressions with undefined and null values', () => {
+    const updatedItem = {
+      name: 'test1',
+      description: null,
+      price: undefined
+    }
+
+    const updateExpressions = createUpdateExpressions(updatedItem)
+    const expected = {
+      UpdateExpression: 'SET #name = :name, #description = :description',
+      ExpressionAttributeValues: {
+        ':name': 'test1',
+        ':description': null
+      },
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#description': 'description'
       }
     }
     expect(updateExpressions).toEqual(expected)
